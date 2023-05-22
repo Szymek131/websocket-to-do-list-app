8bf24d717d7e675c02cfb3f1ebafc9f877c7e382
Szymek131 <sz.wiktor131@gmail.com>
2023-05-22 18:45:03 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import shortid from 'shortid';
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
+  const socket = io('localhost:8000');
+
+  useEffect(() => {
+    socket.on('addTask', tasks => addTask(tasks));
+    socket.on('removeTask', taskId => removeTask(taskId));
+    socket.on('updateData', tasksList => updateTasks(tasksList));
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const updateTasks = tasks => {
+    setTasks(tasks);
+  }
+
+  const removeTask = id => {
+    setTasks(tasks => tasks.filter(task => task.id !== id));
+    socket.broadcast.emit('removeTask', id);
+  }
+
+  const submitForm = e => {
+    e.preventDefault();
+    const taskData = { name: taskName, id: shortid.generate() }
+    addTask(taskData);
+    socket.emit('addTask', taskData);
+  }
+
+  const addTask = task => {
+    setTasks(tasks => [...tasks, task]);
+  }
+
+  return (
+    <div className="App">
+
+      <header>
+        <h1>ToDoList.app</h1>
+      </header>
+
+      <section className="tasks-section" id="tasks-section">
+        <h2>Tasks</h2>
+
+        <ul className="tasks-section__list" id="tasks-list">
+          {tasks.map(task => (
+            <li key={task.id} className="task">{task.name} <button onClick={() => removeTask(task.id)} className="btn btn--red">Remove</button></li>
+          ))}
+        </ul>
+
+        <form id="add-task-form" onSubmit={submitForm}>
+          <input onChange={e => setTaskName(e.target.value)} className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" />
+          <button className="btn" type="submit">Add</button>
+        </form>
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+
+let tasks = [
+];
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
+
+const server = app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id - ' + socket.id);
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    task.push(task);
+    socket.broadcast.emit('addTask', task);
+    console.log('Succesfully added task!');
+  });
+
+  socket.on('removeTask', (taskId) => {
+    const index = tasks.findIndex(item => item.id === taskId);
+    tasks.splice(index, 1);
+    console.log('Task removed!');
+    socket.broadcast.emit('removeTask', taskId);
+  });
+});
